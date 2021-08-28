de64481a1a024149e347b8bdf2a2f3a272d3e140
segatt0 <anabellesegatto@hotmail.com>
2021-08-28 20:03:01 -0300

    28/08/21

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+import img1 from './../../../src/Assets/img1.jpg';
+
+function Login() {
+  return (
+    <div id="loginScreen">
+      <div className="left">
+        <h1>Login Screen</h1>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+}
+
+export default Login;
